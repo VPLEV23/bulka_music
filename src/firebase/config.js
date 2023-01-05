a7d600d8f4423141ff41d9b9731a61806dcb0ded
Vlad Bulgakov <48432102+VPLEV23@users.noreply.github.com>
2023-01-05 22:42:56 +0200

    Firebase setup + firebase composables

---
+++ src/firebase/config.js
@@ -0,0 +1,23 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBxuyTCf30hA0wo2Hz0Ricyk2s7bHCwQV4",
+  authDomain: "bulka-music.firebaseapp.com",
+  projectId: "bulka-music",
+  storageBucket: "bulka-music.appspot.com",
+  messagingSenderId: "719446206294",
+  appId: "1:719446206294:web:8b9a57061ca59f1e83d8f7",
+};
+
+firebase.initializeApp(firebaseConfig);
+
+// init services
+const projectFirestore = firebase.firestore();
+const projectAuth = firebase.auth();
+
+// timestamp
+const timestamp = firebase.firestore.FieldValue.serverTimestamp;
+
+export { projectFirestore, projectAuth, timestamp };
