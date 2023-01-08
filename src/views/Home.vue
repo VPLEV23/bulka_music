b688bc4a9ea0e8d92c3c2a411f0937aa6547ebed
Vlad Bulgakov <48432102+VPLEV23@users.noreply.github.com>
2023-01-09 00:57:41 +0200

    Listing Playlist Component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,15 @@
 <template>
   <div class="home">
-    <p>Hello World</p>
+    <div class="error" v-if="error">Could not fetch the data</div>
+    <div v-if="documents">
+      <ListView :playlists="documents" />
+    </div>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
+<script setup>
+import getCollection from "../composables/getCollection";
+import ListView from "../components/ListView.vue";
 
-export default {
-  name: "Home",
-};
+const { error, documents } = getCollection("playlists");
 </script>
