81d66d39fe63171594204780daaf33cf391a46d4
Vlad Bulgakov <48432102+VPLEV23@users.noreply.github.com>
2023-01-09 12:20:22 +0200

    NavBar Update + Route Guard

--- src/router/index.js
+++ src/router/index.js
@@ -3,11 +3,25 @@ import Home from "../views/Home.vue";
 import Login from "@/views/auth/Login.vue";
 import Register from "@/views/auth/SignUp.vue";
 import CreatePlaylist from "@/views/playlist/CreatePlaylist.vue";
+
+// route guard
+import { projectAuth } from "../firebase/config";
+
+const reqAuth = (to, from, next) => {
+  let user = projectAuth.currentUser;
+  if (!user) {
+    next({ name: "Login" });
+  } else {
+    next();
+  }
+};
+
 const routes = [
   {
     path: "/",
     name: "Home",
     component: Home,
+    beforeEnter: reqAuth,
   },
   {
     path: "/login",
@@ -23,6 +37,7 @@ const routes = [
     path: "/playlists/create",
     name: "CreatePlaylist",
     component: CreatePlaylist,
+    beforeEnter: reqAuth,
   },
 ];
 
